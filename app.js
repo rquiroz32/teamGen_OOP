e3f4072291c5c6fe97f63e198d118f7fca92c5d9
rquiroz3290 <rquiroz3290@gmail.com>
2021-02-14 00:42:40 -0500

    almost finished app, need to figure out how to generate html and save to file

--- app.js
+++ app.js
@@ -4,15 +4,224 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require('./lib/htmlRenderer')
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
+let employees = []
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'Name',
+        message: "What is the name of your Team's Manager you'd like to create?"
+    },
+    {
+        type: 'input',
+        name: 'Id',
+        message: "What is the id of your Team's Manager?"
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: "What is the email of your Team's Manager?"
+    },
+    {
+        type: 'input',
+        name: 'Office Number',
+        message: "What is the office number of the Manager you'd like to create?"
+    },
+    {
+        type: 'list',
+        name: 'Role',
+        message: "What role would you like your Employee to have?",
+        choices: ["Manager"]
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'Name',
+        message: "What is the name of the Engineer you'd like to create?"
+    },
+    {
+        type: 'input',
+        name: 'Id',
+        message: "What is the id of the Engineer?"
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: "What is the email of the Engineer?"
+    },
+    {
+        type: 'input',
+        name: 'GitHub Account Name',
+        message: "What is the GitHub Account Name of the Engineer you'd like to create?"
+    },
+    {
+        type: 'list',
+        name: 'Role',
+        message: "What role would you like your Employee to have?",
+        choices: ["Engineer"]
+    }
+
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'Name',
+        message: "What is the name of the Intern you'd like to create?"
+    },
+    {
+        type: 'input',
+        name: 'Id',
+        message: "What is the id of the Intern?"
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: "What is the email of the Intern?"
+    },
+    {
+        type: 'input',
+        name: 'School',
+        message: "What School does the Intern you'd like to create attend?"
+    },
+    {
+        type: 'list',
+        name: 'Role',
+        message: "What role would you like your Employee to have?",
+        choices: ["Intern"]
+    }
+
+]
+
+function writeToFile(fileName, data) {
+        fs.writeFile(fileName, render(data), err => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(`Successfully wrote html`)
+    })
+}
+
+
+function askManagerQs() {
+    inquirer.prompt(managerQuestions).then((data) => {
+        let newManager = new Manager(data.Name,data.Id,data.Email,data.Role)
+        employees.push(newManager)
+
+        inquirer.prompt(employeeTypeQs).then((data) => {
+            routeQuestions(data)
+        })
+    })
+}
+
+function askEngineerQs() {
+    inquirer.prompt(engineerQuestions).then((data) => {
+        let newEngineer = new Engineer(data.Name,data.Id,data.Email,data.Role)
+        employees.push(newEngineer)
+        employees.push(data)
+        inquirer.prompt(employeeTypeQs).then((data) => {
+            routeQuestions(data)
+        })
+    })
+}
+
+function askInternQs() {
+    inquirer.prompt(internQuestions).then((data) => {
+        let newIntern = new Intern(data.Name,data.Id,data.Email,data.Role)
+        employees.push(newIntern)        
+        inquirer.prompt(employeeTypeQs).then((data) => {
+            routeQuestions(data)
+        })
+    })
+}
+
+
+const initialQuestions = [
+    {
+        type: "confirm",
+        name: "Proceed",
+        message: "Welcome to the Team Generator, please enter \'y\' to proceed with creating your team. "
+
+    }
+
+]
+
+const employeeTypeQs = [
+    {
+        type: "list",
+        name: "EmployeeType",
+        message: "What type of Employee would you like to create next?",
+        choices: ["Manager", "Engineer", "Intern", "None (exit)"]
+    }
+]
+
+function routeQuestions(data) {
+    console.log(data)
+    if (data.EmployeeType === 'Engineer') {
+        askEngineerQs()
+    }
+    else if (data.EmployeeType === 'Intern') {
+        askInternQs()
+    }
+    else if (data.EmployeeType === 'Manager') {
+        askManagerQs()
+    }
+    else if (data.EmployeeType === 'None (exit)') {
+      console.log(employees)
+    }
+
+
+}
+
+
+
+function quit() {
+    console.log(render(employees))
+    
+    if (employees){
+        let HTML = render(employees)
+        
+        // let fileName = `${data.title.toLowerCase().split(' ').join('')}.HTML`
+        writeToFile("filename.html", HTML)
+    }    
+
+    console.log("Goodbye!");
+
+    process.exit();
+}
+
+
+
+function init() {
+    inquirer.prompt(initialQuestions).then((data) => {
+        console.log(data)
+
+        if (data.Proceed === true) {
+            askManagerQs()
+        } else {
+            quit()
+        }
+
+    })
+}
+
+init();
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
